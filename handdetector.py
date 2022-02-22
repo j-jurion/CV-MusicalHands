dc000390b6a81ff00c6644ae9c8ad546ea782910
j-jurion <51537901+j-jurion@users.noreply.github.com>
2022-02-22 21:28:13 +0100

    L and R hand distinction

--- handdetector.py
+++ handdetector.py
@@ -1,16 +1,16 @@
-import cv2
+import cv2 as cv
 import mediapipe as mp
 import time
 
 
 class HandDetector():
-  def __init__(self):
+  def __init__(self) -> None:
     self.mpHands = mp.solutions.hands
     self.hands = self.mpHands.Hands()
     self.mpDraw = mp.solutions.drawing_utils
   
   def findHands(self, img):
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     self.results = self.hands.process(imgRGB)
     if self.results.multi_hand_landmarks:
       for handLms in self.results.multi_hand_landmarks:
@@ -18,17 +18,47 @@ class HandDetector():
 
     return img
 
-  def findPosition(self, img, fingerNo):
+  def findPosition(self, img, leftHand: bool, fingerNo: int) -> list:
+    if leftHand: handstr, coordinates, color = "L", (50,100), (0,0,255)
+    else:  handstr, coordinates, color = "R", (450,100),(255,0,0)
+
     lmList = []
     if self.results.multi_hand_landmarks:
-      for hand in self.results.multi_hand_landmarks:
+      hand = self.findHand(leftHand)
+      if hand:
+        cv.putText(img, text=handstr, org=coordinates, fontFace=cv.FONT_HERSHEY_TRIPLEX, fontScale=3, color=color,thickness=3)
         for id, lm in enumerate(hand.landmark):
           if id == fingerNo:
             h, w, ch = img.shape
             cx, cy = int(lm.x*w), int(lm.y*h) ;#center coordinates
             lmList.append([cx, cy])
-            cv2.circle(img, (cx, cy), 7, (255,0,255), cv2.FILLED)
-    
+            cv.circle(img, (cx, cy), 7, color, cv.FILLED)
+
     return lmList
+
+  def findHand(self, left: bool):
+    hands = self.results.multi_hand_landmarks
+
+    # Only one hand detected
+    if len(hands) == 1:
+      if left:
+        if hands[0].landmark[8].x > hands[0].landmark[20].x:
+          return hands[0]
+        else: return None
+      else:
+        if hands[0].landmark[8].x < hands[0].landmark[20].x:
+          return hands[0]
+        else: return None
+
+    # Two hands detected
+    else:
+      if left:
+        if hands[0].landmark[0].x < hands[1].landmark[0].x:
+          return hands[0]
+        else: return hands[1]
+      else:
+        if hands[0].landmark[0].x < hands[1].landmark[0].x:
+          return hands[1]
+        else: return hands[0]
 
   
--- theremin.py
+++ theremin.py
@@ -27,24 +27,35 @@ while True:
   success, img = cap.read()
   img = cv.flip(img, 1)
   hand.findHands(img)
-  f4 = hand.findPosition(img, 4)
-  f8 = hand.findPosition(img, 8)
-  sound_param = [] ;#Frequency, volume
+  #f4 = hand.findPosition(img, 4)
+  #f8 = hand.findPosition(img, 8)
+  
+  #TODO test
+  frequency_handpos_4 = hand.findPosition(img, True, 4)
+  frequency_handpos_8 = hand.findPosition(img, True, 8)
+  volume_handpos_4 = hand.findPosition(img, False, 4)
+  volume_handpos_8 = hand.findPosition(img, False, 8)
+  #print(f"frequency:    {frequency_handpos}          |    volume:    {volume_handpos}")
+
   try: 
-    cv.line(img, (f4[0][0], f4[0][1]), (f8[0][0], f8[0][1]), (0, 255, 0), thickness=3, lineType=8)
-    frequency = math.sqrt((f8[0][0] - f4[0][0])**2 + (f8[0][1] - f4[0][1])**2)
+    cv.line(img, (frequency_handpos_4[0][0], frequency_handpos_4[0][1]), (frequency_handpos_8[0][0], frequency_handpos_8[0][1]), (0, 255, 0), thickness=3, lineType=8)
+    frequency = math.sqrt((frequency_handpos_8[0][0] - frequency_handpos_4[0][0])**2 + (frequency_handpos_8[0][1] - frequency_handpos_4[0][1])**2)
   except IndexError:
     frequency = 440
   try: 
-    cv.line(img, (f4[1][0], f4[1][1]), (f8[1][0], f8[1][1]), (0, 255, 0), thickness=3, lineType=8)
-    volume = math.sqrt((f8[1][0] - f4[1][0])**2 + (f8[1][1] - f4[1][1])**2)
+    cv.line(img, (volume_handpos_4[0][0], volume_handpos_4[0][1]), (volume_handpos_8[0][0], volume_handpos_8[0][1]), (0, 255, 0), thickness=3, lineType=8)
+    volume = math.sqrt((volume_handpos_8[0][0] - volume_handpos_4[0][0])**2 + (volume_handpos_8[0][1] - volume_handpos_4[0][1])**2)
   except IndexError:
     volume = 0
 
-  volume = volume_map(volume)
-  frequency = frequency_map(frequency)
+
+
+  #sound_param = [] ;#Frequency, volume
+
+  #volume = volume_map(volume)
+  #frequency = frequency_map(frequency)
   #print(f"frequencyRaw: {sound_param[0]}     |    volumeRaw: {sound_param[1]}")
-  print(f"frequency:    {frequency}          |    volume:    {volume}")
+  #print(f"frequency:    {frequency}          |    volume:    {volume}")
 
 
   #sound.play(440, volume)
